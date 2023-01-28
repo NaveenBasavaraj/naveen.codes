eb69957569b56c2dbf62ca2792f605ecbcd944f1
Naveen Basavaraj <61464089+NaveenBasavaraj@users.noreply.github.com>
2023-01-28 17:48:26 +0000

    doubly linked list

--- 1_Trees/recursive_bst.py
+++ 1_Trees/recursive_bst.py
@@ -2,5 +2,11 @@ from tree import BinaryTree
 
 
 class recursive_BST(BinaryTree):
+
   def __r_contains(self, curr_node, value):
-    
+    if curr_node == None:
+      return False
+    if value == curr_node.value:
+      return True
+    if value < curr_node.value:
+      return self.__r_contains(curr_node.left, value)
