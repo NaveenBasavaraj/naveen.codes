8908a6c0e0c93893a580f32e00685c037f90dc6a
Naveen Basavaraj <61464089+NaveenBasavaraj@users.noreply.github.com>
2023-03-15 02:41:56 +0000

    find prime

--- 0_maths_&_bit_&_strings/2.math algorithms/2_find_if_a_no_is_prime.py
+++ 0_maths_&_bit_&_strings/2.math algorithms/2_find_if_a_no_is_prime.py
@@ -1,7 +1,44 @@
-class findIfPrime:
+class CheckPrime:
+  '''
+  given a number find if its prime
+  '''
 
-  def squareRootMethod(self, n):
-    pass
+  def is_prime(self, num):
+    if num < 2 or num % 2 == 0:
+      # for negative or if can be divided by 2
+      return False
+    elif num == 2:
+      # 2 is prime
+      return True
+    else:
+      for i in range(3, int(num**0.5) + 1, 2):
+        # it is enough to check till sqrt of num
+        # step size is kept 2 so that we check only odds
+        if num % i == 0:
+          return False
+        return True
 
-  def badMethod(self, n):
-    pass
+  def is_prime_no_sqrt(self, num):
+    if num < 2 or num % 2 == 0:
+      # for negative or if can be divided by 2
+      return False
+    elif num == 2:
+      # 2 is prime
+      return True
+    else:
+      for i in range(3, num, 2):
+        # step size is kept 2 so that we check only odds
+        if num % i == 0:
+          return False
+        return True
+
+
+checker = CheckPrime()
+
+num1 = 23
+num2 = 10
+
+print(f"{num1} is prime: {checker.is_prime(num1)}")
+print(f"{num2} is prime: {checker.is_prime(num2)}")
+print(f"{num1} is prime: {checker.is_prime_no_sqrt(num1)}")
+print(f"{num2} is prime: {checker.is_prime_no_sqrt(num2)}")
--- main.py
+++ main.py
@@ -1,59 +1,44 @@
-# Find all primes upto given number N
+class CheckPrime:
+  '''
+  given a number find if its prime
+  '''
+
+  def is_prime(self, num):
+    if num < 2 or num % 2 == 0:
+      # for negative or if can be divided by 2
+      return False
+    elif num == 2:
+      # 2 is prime
+      return True
+    else:
+      for i in range(3, int(num**0.5) + 1, 2):
+        # it is enough to check till sqrt of num
+        # step size is kept 2 so that we check only odds
+        if num % i == 0:
+          return False
+        return True
+
+  def is_prime_no_sqrt(self, num):
+    if num < 2 or num % 2 == 0:
+      # for negative or if can be divided by 2
+      return False
+    elif num == 2:
+      # 2 is prime
+      return True
+    else:
+      for i in range(3, num, 2):
+        # step size is kept 2 so that we check only odds
+        if num % i == 0:
+          return False
+        return True
 
 
-class countPrimes:
+checker = CheckPrime()
 
-  def countPrime(self, n):
-    """
-    A function to count the number of prime numbers less than a given number n using a brute-force approach.
+num1 = 23
+num2 = 10
 
-    Args:
-      n (int): A positive integer.
-
-    Returns:
-      int: The number of prime numbers less than n.
-
-    Example:
-      >>> obj = countPrimes()
-      >>> obj.countPrime(10)
-      4
-    """
-    if n <= 1:
-      return 0
-    count = 0
-    for i in range(2, n + 1):
-      for j in range(2, int(i**0.5) + 1):
-        if not i % j == 0:
-          count += 1
-    return count
-
-  def seiveOfErathosthenes(self, n):
-    """
-    A function to count the number of prime numbers less than a given number n using the Sieve of Eratosthenes method.
-
-    Args:
-      n (int): A positive integer.
-
-    Returns:
-      int: The number of prime numbers less than n.
-
-    Example:
-      >>> obj = countPrimes()
-      >>> obj.seiveOfErathosthenes(20)
-      8
-    """
-    if n <= 1:
-      return 0
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False
-
-    for i in range(2, int(n**0.5) + 1):
-      if is_prime[i]:
-        for j in range(i * i, n + 1, i):
-          is_prime[j] = False
-    return sum(is_prime)
-
-
-obj = countPrimes()
-print(obj.seiveOfErathosthenes(30))
-
+print(f"{num1} is prime: {checker.is_prime(num1)}")
+print(f"{num2} is prime: {checker.is_prime(num2)}")
+print(f"{num1} is prime: {checker.is_prime_no_sqrt(num1)}")
+print(f"{num2} is prime: {checker.is_prime_no_sqrt(num2)}")
